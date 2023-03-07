58cfd1a34f3096ae6e48158d5e347223850d8677
balu287 <subramanyam287@gmail.com>
2023-03-07 12:35:17 +0000

    removed test cases

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -16,31 +16,4 @@ public class CalculatorTest {
     assertEquals(Calculator.add(-4, 2.0), -2.0, DELTA);
   }
 
-  @Test
-  public void testSubtract() {
-    assertEquals(Calculator.subtract(1, 2), -1.0, DELTA);
-    assertEquals(Calculator.subtract(2, 1), 1.0, DELTA);
-    assertEquals(Calculator.subtract(1.0, 2.0), -1.0, DELTA);
-    assertEquals(Calculator.subtract(0, 2.0), -2.0, DELTA);
-    assertEquals(Calculator.subtract(2.0, 0), 2.0, DELTA);
-    assertEquals(Calculator.subtract(-4, 2.0), -6.0, DELTA);
-  }
-
-  @Test
-  public void testMultiply() {
-    assertEquals(Calculator.multiply(1, 2), 2.0, DELTA);
-    assertEquals(Calculator.multiply(1.0, 2.0), 2.0, DELTA);
-    assertEquals(Calculator.multiply(0, 2.0), 0.0, DELTA);
-    assertEquals(Calculator.multiply(2.0, 0), 0.0, DELTA);
-    assertEquals(Calculator.multiply(-4, 2.0), -8.0, DELTA);
-  }
-
-  @Test
-  public void testDivide() {
-    assertEquals(Calculator.divide(1, 2), 0.5, DELTA);
-    assertEquals(Calculator.divide(1.0, 2.0), 0.5, DELTA);
-    assertEquals(Calculator.divide(0, 2.0), 0, DELTA);
-    assertEquals(Calculator.divide(-4, 2.0), -2.0, DELTA);
-    // assertEquals(Calculator.divide(2.0, 0), 0.0, DELTA);
-  }
 }
